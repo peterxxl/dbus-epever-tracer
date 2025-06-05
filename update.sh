b05485c8aa024e6f83c6977cffe3287d2157c8e6
Peter <peter@cekyl.com>
2025-06-05 21:07:37 +0200

    refactor: improve update script with progress indicators and better error handling

--- update.sh
+++ update.sh
@@ -1,36 +1,51 @@
 #!/bin/bash
 
-read -p "Update Epever Solarcharger on Venus OS at your own risk? [Y to proceed]" -n 1 -r
-echo    # (optional) move to a new line
+# ===============================
+# Epever Tracer Venus OS Updater
+# ===============================
+# This script updates the Epever Tracer driver and its dependencies on Venus OS.
+# It will download the latest driver and Victron Python library, install them,
+# and set up permissions and symlinks. Use at your own risk!
+
+read -p "Update Epever Tracer on Venus OS at your own risk? [Y to proceed]" -n 1 -r
+echo    # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	echo "Download driver and library"
+    echo "\n[1/6] Switching to /data directory..."
+    cd /data
 
-	cd /data
+    echo "[2/6] Downloading latest dbus-epever-tracer driver..."
+    wget -q --show-progress https://github.com/peterxxl/dbus-epever-tracer/archive/master.zip
+    echo "Unzipping driver..."
+    unzip -q master.zip
+    rm master.zip
 
-	wget https://github.com/peterxxl/dbus-epever-tracer/archive/master.zip
-	unzip master.zip
-	rm master.zip
+    echo "[3/6] Downloading latest Victron velib_python library..."
+    wget -q --show-progress https://github.com/victronenergy/velib_python/archive/master.zip
+    echo "Unzipping library..."
+    unzip -q master.zip
+    rm master.zip
 
-	wget https://github.com/victronenergy/velib_python/archive/master.zip
-	unzip master.zip
-	rm master.zip
+    echo "[4/6] Installing libraries and organizing folders..."
+    mkdir -p dbus-epever-tracer/ext/velib_python
+    cp -R dbus-epever-tracer-master/* dbus-epever-tracer
+    cp -R velib_python-master/* dbus-epever-tracer/ext/velib_python
 
-	mkdir -p dbus-epever-tracer/ext/velib_python
-    	cp -R dbus-epever-tracer-master/* dbus-epever-tracer
-	cp -R velib_python-master/* dbus-epever-tracer/ext/velib_python
+    echo "Cleaning up temporary files..."
+    rm -r velib_python-master
+    rm -r dbus-epever-tracer-master
 
-	rm -r velib_python-master
-	rm -r dbus-epever-tracer-master
+    echo "[5/6] Setting execute permissions on driver and service scripts..."
+    chmod +x /data/dbus-epever-tracer/driver/start-dbus-epever-tracer.sh
+    chmod +x /data/dbus-epever-tracer/driver/dbus-epever-tracer.py
+    chmod +x /data/dbus-epever-tracer/service/run
+    chmod +x /data/dbus-epever-tracer/service/log/run
 
-	echo "Install driver"
-	chmod +x /data/dbus-epever-tracer/driver/start-dbus-epever-tracer.sh
-	chmod +x /data/dbus-epever-tracer/driver/dbus-epever-tracer.py
-	chmod +x /data/dbus-epever-tracer/service/run
-	chmod +x /data/dbus-epever-tracer/service/log/run
+    echo "[6/6] Creating/Updating symlinks for Victron Energy integration..."
+    ln -sf /data/dbus-epever-tracer/driver /opt/victronenergy/dbus-epever-tracer
+    ln -sf /data/dbus-epever-tracer/service /opt/victronenergy/service-templates/dbus-epever-tracer
 
-	ln -s /data/dbus-epever-tracer/driver /opt/victronenergy/dbus-epever-tracer
-	ln -s /data/dbus-epever-tracer/service /opt/victronenergy/service-templates/dbus-epever-tracer
-
-	echo "To finish, reboot the Venus OS device"
+    echo "\nUpdate complete! To finish, please reboot your Venus OS device."
+else
+    echo "\nUpdate cancelled by user. No changes were made."
 fi
